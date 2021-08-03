20dd40ee7e104741f05147f515fb7b0245132351
T Sandler <toddsandlerwasd@gmail.com>
2021-08-03 23:48:48 +0100

    adding web app to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
+import Recipe from './recipe';
 import './App.css';
 
-function App() {
-  return (
+
+const App = () =>{
+
+  const APP_ID = 'bef8fa8d'
+  const APP_KEY = 'e6a9f12405ea771a77d6042ee567d218'
+
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
+
+  useEffect(() => {
+    getRecipes()
+  }, [query])
+
+  const getRecipes = async () =>{
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+    setRecipes(data.hits)
+  }
+
+  const updateSeatch = e => {
+    setSearch(e.target.value)    
+  }
+
+  const getSearch = e =>{
+    e.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSeatch}/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <div className="recipies">
+      {recipes.map(recipe =>(
+        <Recipe
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients} />
+      ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
